729d8ba38f3af8c8ea5d9b67a796d11282947e3f
Seth Zyg <zygster11@gmail.com>
2021-01-12 20:45:48 -0600

    Nearly finished. Code is all working. Styling finished and resembling demo. Working on finishing README file.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,9 +1,64 @@
-var saveButtons = $('.saveBtn')
+//declare a variable set to the button elements in the HTML
+var saveButtons = $('button');
+//declare a variable for the p element that will house the current date
+var currentDay = $('#currentDay');
+//declare a variable for the moment() function (in hindsight, this wasn't really necessary)
+var today = moment();
+
+//declare a variable that is an array of all of the textareas in the document
+var hourText = $('textarea').toArray();
+           
+//Appends the current date grabbed from the moment API into the the appropriate div.
+currentDay.append(moment().format('dddd, MMMM Do, YYYY'));
+ /*'dddd' gets the current day in string form, MMMM gets the current month in string form, Do gets the day of the month
+ in int form, and YYYY gets the current year in int form (eg 2021) */
 
 
-saveButtons.on('click', saveHourData());
+//when a save button is clicked...
+saveButtons.on('click', function(){
+//for every item in the hourText array we just created...
+  for (var i = 0; i < hourText.length; i++) {
+    //if the save button we clicked has an id that matches a certain row, that means the save button is also on that row
+    if (($(this).attr('id')) === $(hourText[i]).attr('id')) {
+      //so ultimately when we hit 'save', we send whatever text is in that row's textarea to local storage
+      localStorage.setItem("hour: "+$(hourText[i]).attr('id'), $(hourText[i]).val());
+    }
+  }  
+})
+  //name a function for determining which hours on the planner are in the past, in the future, or the present hour
+  function pastPresentFuture () {
+    //so for each of our classes labeled 'row' (which is every row in the planner), execute the following function:
+    $('textarea').each( function() { 
+      //if the integer value (parseInt()) of the 'value' attribute I set for the rows is < the integer value for the current hour (e.g. noon = 12)
+      if ((parseInt($(this).attr('value'))) < (parseInt(today.format('HH')))) {
+        //then for this particular row, we will label it in the past, which changes its formatting
+        $(this).attr('class', 'past');
+      }
+      //if the integer value of the 'value' attr is GREATER than the integer value for the current hour, we add the 'future' class
+      else if (((parseInt($(this).attr('value'))) > (parseInt(today.format('HH'))))) {
+        $(this).attr('class', 'future');
+      }
+      //if the integer value for the current row's value attr is the same as the current hour, we add the 'present' class to 
+      else if (((parseInt($(this).attr('value'))) === (parseInt(today.format('HH'))))) {
+        $(this).attr('class', 'present')
+   }
+  })
+} 
 
-function saveHourData (){
-    var userInput = $('hourInput')
-    localStorage.setItem("input", userInput)
+function onRefresh() {
+//for every item in our array of hourly textareas we created globally...
+  for (var i = 0; i < hourText.length; i++) {
+    //...if a key for that specific hour of the day exists in local storage... 
+    if (localStorage.getItem("hour: "+$(hourText[i]).attr('id'))) {    
+
+    //...populate the textarea with the value associated with that key in local storage
+    //this does indeed work and keeps text on refresh, but it doesn't FEEL right lol. An inelegant solution perhaps.
+     $(hourText[i]).val(localStorage.getItem("hour: "+$(hourText[i]).attr('id')));
+    }
+  }
 }
+
+//call pastPresentFuture right away so page will be nicely color-coded on loading
+pastPresentFuture(); 
+//similarly, call the onRefresh function so that anytime the page is refreshed it executes
+onRefresh();
---
+++ MyREADME.md
@@ -0,0 +1,23 @@
+# Day Planner - Homework 5
+
+A simple application for planning your day by the hour.
+
+# Purpose of the Repository
+
+This fifth homework assignment tasked the coder with creating a functional day planner for the current day of the week. It was designed to challenge the coder to utilize jQuery to store information from the planner page in local storage and keep information placed by the user on the page after the page is refreshed. The assignment also called for usage of a third-party API, called 'moment.js' to be implemented in the day planner to set the current day of the week. 
+
+# Description of Repository 
+
+The repository creates a functioning day planner. The planner consists of rows designating the hours of the current 'work' day, from 9 am to 6 pm. On loading the page, it retrieves the current month, current day of the month, current day of the week, and current year will, which is displayed just above the the calendar. A third-party API named 'moment.js' is used to retrieve this current date. 
+
+In the body of the page, an array of rows displays the hours for the current day. Each of these rows has text to its left designating which hour of the day it is. To the right of this rests a textarea element in which the user can input any relevant data related to that hour (e.g. make coffee at 9am, eat lunch at 12pm, etc.). A save button sits to the right of textarea, and once clicked, it will save whatever text was placed in the textarea to local storage. The save button only saves data for the specific row it corresponds to. 
+
+The user can place multiple inputs in different rows and save each of them using the appropriate save button. The local storage then will become populated with the text placed by the user. A new key is made for each hour (e.g. the 9am row's key is "hour 9"). If the user refreshes the page, the input they have placed in the textareas will remain there until edited or removed by the user. 
+
+Another feature of this day planner is color-coordination. Any hour-rows that are BEFORE the current hour (i.e. hours in the past) will have a slate gray background-color to them. The present hour will be coded a nice salmon color, while future hours will have a soothing pastel green hue to them. This functionality required using the moment.js API to compare the current hour on load with the values of each row. 
+
+# Files in Repository
+
+This repository was sort of unique in that style.css file was provided for the coder and needed only a few alterations. This style.css file I moved into the 'css' folder I created inside of the 'Assets' folder. Also in the Assets folder one can find the js folder housing the script.js file, in which lies my jQuery code for the assignment, as well as the provided gif image for how the day planner is supposed to look. I think mine came pretty close. 
+
+The index.html file is outside of any folder branches, along with the MyREADME file. 
